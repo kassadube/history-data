f545b1f562a72abc1bdafc6a2901c71290f77d48
kassadube <noamsrb@yahoo.com>
2021-06-22 17:29:59 +0300

    update as function

--- app.py
+++ app.py
@@ -6,31 +6,41 @@ import matplotlib.pyplot as plt    # plot data
 import qgrid                       # display dataframe in notebooks 
 
 
-url = "https://api.binance.com/api/v3/klines"
-interval = '1h'
-symbol = 'ETHUSDT'
-startTime = str(int(dt.datetime(2020, 1, 1).timestamp() * 1000))
-endTime = str(int(dt.datetime(2020, 2, 1).timestamp() * 1000))
-limit = '1000'
 
-req_params = {"symbol" : symbol, 'interval' : interval, 'startTime' : startTime, 'endTime' : endTime, 'limit' : limit}
-df = pd.DataFrame(json.loads(requests.get(url, params = req_params).text))
+
+
+def get_binance_bars(symbol, interval, startTime, endTime):
+     
+    url = "https://api.binance.com/api/v3/klines"
  
-#if (len(df.index) == 0):
- #   return None
+    startTime = str(int(startTime.timestamp() * 1000))
+    endTime = str(int(endTime.timestamp() * 1000))
+    limit = '1000'
+ 
+    req_params = {"symbol" : symbol, 'interval' : interval, 'startTime' : startTime, 'endTime' : endTime, 'limit' : limit}
+ 
+    df = pd.DataFrame(json.loads(requests.get(url, params = req_params).text))
+ 
+    if (len(df.index) == 0):
+        return None
+     
+    df = df.iloc[:, 0:6]
+    df.columns = ['datetime', 'open', 'high', 'low', 'close', 'volume']
+ 
+    df.open      = df.open.astype("float")
+    df.high      = df.high.astype("float")
+    df.low       = df.low.astype("float")
+    df.close     = df.close.astype("float")
+    df.volume    = df.volume.astype("float")
     
-df = df.iloc[:, 0:6]
-df.columns = ['datetime', 'open', 'high', 'low', 'close', 'volume']
+    df['adj_close'] = df['close']
+     
+    df.index = [dt.datetime.fromtimestamp(x / 1000.0) for x in df.datetime]
+ 
+    return df
 
-df.open      = df.open.astype("float")
-df.high      = df.high.astype("float")
-df.low       = df.low.astype("float")
-df.close     = df.close.astype("float")
-df.volume    = df.volume.astype("float")
 
-df['adj_close'] = df['close']
-    
-df.index = [dt.datetime.fromtimestamp(x / 1000.0) for x in df.datetime]
+df = get_binance_bars('ETHUSDT', '1h', dt.datetime(2020, 1, 1), dt.datetime(2020, 2, 1))
+
 print(df)
-df.to_csv("foo.csv")
-df['close'].astype('float').plot()
+df.to_csv("fo1.csv")
